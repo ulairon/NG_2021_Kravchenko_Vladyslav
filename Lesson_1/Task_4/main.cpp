cf7f328433f3c8503b8f01a2ee69fc95bdeb34eb
Kravchenko Vladyslav <wwmoonrage@gmail.com>
2021-07-12 13:36:48 +0300

    Fixed bug for Task

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -8,7 +8,9 @@ int main()
     cout << "Skazhite vashu zarplatu v $, pozhatusta: ";
     cin >> ZP;
     if (ZP<1000) { cout << "Rabotai bolshe!"; }
-    if (1000000 > ZP > 999) { cout << "Kruto"; }
+    if (ZP > 999) {
+        if (ZP < 1000000){ cout << "Kruto"; }
+    }
     if (ZP > 999999) { cout << "Da ti millioner"; }
     cout << ", no ti molodec!" << endl;
     return 0;
