336dac1fdc8fa759829ad88b735d5435cd6a1ce4
Kravchenko Vladyslav <wwmoonrage@gmail.com>
2021-07-14 15:34:18 +0300

    Fixed names for variables and code. char for variable with name For_Switch not changed because it isn't work with int.

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -4,37 +4,38 @@ using namespace std;
 
 int main()
 {
-    int PCh, VCh, R;
-    char D;
+    int PervoeChislo, VtoroeChislo, Result;
+    char For_Cases;
+    
     cout << "vvedite pervoe chislo: " ;
-    cin >> PCh;
+    cin >> PervoeChislo;
+    
     cout << "vvedite vtoroe chislo: " ;
-    cin >> VCh;
+    cin >> VtoroeChislo;
+    
     cout << "vvedite svoe deistvie (1- dobavit, 2- otnimanie, 3- umnozhenie, 4- delenie.): " ;
-    cin >> D;
-    switch (D)
+    cin >> For_Cases;
+
+    switch (For_Cases)
     {
         case '1' :
-        R = PCh + VCh;
+        Result = PervoeChislo + VtoroeChislo ;
         break;
+        
         case '2' :
-        R = PCh - VCh ;
+        Result = PervoeChislo - VtoroeChislo ;
         break;
+        
         case '3' :
-        R = PCh * VCh ;
+        Result = PervoeChislo * VtoroeChislo ;
         break;
+        
         case '4' :
-        R = PCh/VCh ;
+        Result = PervoeChislo / VtoroeChislo ;
         break;
     }
-    cout << "Otvet: " << R << endl ;
-    return 0;
-}
 
-        case '4' :
-        PCh/VCh ;
-        break;
-    }
-    cout << "Otvet: " << R << endl ;
+    cout << "Otvet: " << Result << endl ;
+
     return 0;
 }
