b696e35412b2c0c965a2acb7641501ac657aadea
Kravchenko Vladyslav <wwmoonrage@gmail.com>
2021-07-14 15:12:06 +0300

    fixed logic in code

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -7,11 +7,12 @@ int main()
     int ZP;
     cout << "Skazhite vashu zarplatu v $, pozhatusta: ";
     cin >> ZP;
-    if (ZP<1000) { cout << "Rabotai bolshe!"; }
-    if (ZP > 999) {
+    if (ZP > 999999) { cout << "Da ti millioner"; }
+     if (ZP > 999) {
         if (ZP < 1000000){ cout << "Kruto"; }
     }
-    if (ZP > 999999) { cout << "Da ti millioner"; }
+    if (ZP<1000) { cout << "Rabotai bolshe!"; }
+   
     cout << ", no ti molodec!" << endl;
     return 0;
 }
