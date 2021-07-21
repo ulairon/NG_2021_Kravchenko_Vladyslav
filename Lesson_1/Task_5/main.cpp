42ca403dae57947b846cc0c3e0d8b9af290bf30d
Kravchenko Vladyslav <wwmoonrage@gmail.com>
2021-07-21 11:31:57 +0300

    changed <cout> for <Deistvie> from line to column in Task_5

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 using namespace std;
@@ -6,40 +7,45 @@ int main()
 {
     int PervoeChislo, VtoroeChislo;
     int For_Cases;
-    
+
     cout << "vvedite pervoe chislo: " ;
     cin >> PervoeChislo;
-    
+
     cout << "vvedite vtoroe chislo: " ;
     cin >> VtoroeChislo;
-    
-    cout << "vvedite svoe deistvie" << endl << "1- dobavit" << endl << "2- otnimanie" << endl << "3- umnozhenie" << endl << "4- delenie " << endl ;
+
+    cout << "vvedite svoe deistvie" << endl ;
+    cout<< "1- dobavit" << endl ;
+    cout<< "2- otnimanie" << endl;
+    cout << "3- umnozhenie" << endl ;
+    cout << "4- delenie " << endl << endl ;
+    cout << "Deistvie: " ;
     cin >> For_Cases;
-    
-    cout << "Result: ";
-    
+
+    cout <<endl << endl << "Result: ";
+
     switch (For_Cases)
     {
-    
+
     case 1 :
         cout << PervoeChislo + VtoroeChislo << endl ;
         break;
-        
+
     case 2 :
         cout << PervoeChislo - VtoroeChislo << endl ;
         break;
-        
+
     case 3 :
         cout << PervoeChislo * VtoroeChislo << endl ;
         break;
-        
+
     case 4 :
         cout << PervoeChislo / VtoroeChislo << endl ;
         break;
-        
+
     default :
         cout << "vveden ne pravelniy tip deistviya";
     }
-    
+
     return 0;
 }
