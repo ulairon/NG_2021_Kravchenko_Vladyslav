7f1d2022adeacfe37def9e91c63bac77f45e5acb
Kravchenko Vladyslav <wwmoonrage@gmail.com>
2021-07-30 09:30:01 +0300

    removed variable <for_shirina> and fixed code

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -4,22 +4,19 @@ using namespace std;
 
 int main()
 {
-    int dlina, shirina, for_dlina, for_shirina ;
+    int dlina, for_dlina, shirina ;
 
     cout << "vvedite dlinnu: " ;
     cin >> dlina ;
 
-    cout << "vvedite shirinu: " ;
-    cin >> shirina ;
-
-    if (dlina <= 1 || shirina <= 1)
+    if (dlina <= 1)
     {
 
         cout << "neverno zadano odno iz znacheniy" ;
 
     } else {
 
-        for (for_shirina = 1; for_shirina <= shirina ; for_shirina++ )
+        for (shirina = 1; shirina <= dlina ; shirina++ )
         {
 
             for (for_dlina = 0; for_dlina < dlina; for_dlina++ )
